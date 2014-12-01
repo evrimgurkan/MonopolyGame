0feca0579411ed95babebfb040ac80cb5d377742
evrimgurkan <e.evrimgurkan@gmail.com>
2014-12-01 14:50:08 +0200

    Development #6
    Modified Assets.
    * Added color enum
    * Added two more functions to abstract class

--- src/Monopoly/BusinessLayer/Assets.cs
+++ src/Monopoly/BusinessLayer/Assets.cs
@@ -5,48 +5,41 @@ using System.Text;
 
 namespace BusinessLayer
 {
+    public enum CardColor
+    {
+        Red,
+        Blue,
+        Yellow,
+        Green,
+        Orange,
+        Brown,
+        Cyan,
+        Purple
+    }
+
     abstract class Assets
     {
         public abstract int getRentPrice();
 
         public abstract int getSellingPrice();
 
-        private bool _isMortgaged;
-        public bool isMortgaged
-        {
-            get { return _isMortgaged; }
-            set { _isMortgaged = value; }
-        }
+        public abstract bool isMortgaged();
 
-        private Player _ownedBy;
-        public Player ownedBy
-        {
-            get { return _ownedBy; }
-            set { _ownedBy = value; }
-        }
-
-        public Assets()
-        {
-            _ownedBy = null;
-            _isMortgaged = false;
-        }
+        public abstract Player ownedBy();
     }
 
     class PropertySpace : Assets
     {
-        // TODO: it can be enum for color
-        private int _colorGroup;
-        public int colorGroup
+        private CardColor colorGroup;
+        public CardColor getColorGroup()
         {
-            get { return _colorGroup; }
-            set { _colorGroup = value; }
+            return colorGroup;
         }
 
-        private int _housePrice;
-        public int housePrice
+        private int housePrice;
+        public int getHousePrice()
         {
-            get { return _housePrice; }
-            set { _housePrice = value; }
+            return housePrice;
         }
 
         private int _numberOfHouses;
@@ -68,13 +61,29 @@ namespace BusinessLayer
             return sellingPrice;
         }
 
-        public PropertySpace(int _rentPrice, int _sellingPrice)
+        private bool mortgaged;
+        public override bool isMortgaged()
+        {
+            return mortgaged;
+        }
+
+        private Player owner;
+        public override Player ownedBy()
+        {
+            return owner;
+        }
+
+        public PropertySpace(int _rentPrice, int _sellingPrice, 
+            CardColor _colorGroup, int _housePrice, 
+            bool _mortgaged, Player _owner)
         {
             rentPrice = _rentPrice;
             sellingPrice = _sellingPrice;
+            colorGroup = _colorGroup;
+            housePrice = _housePrice;
+            mortgaged = _mortgaged;
+            owner = _owner;
 
-            _colorGroup = -1;
-            _housePrice = 0;
             _numberOfHouses = 0;
         }
     }
@@ -93,7 +102,20 @@ namespace BusinessLayer
             return sellingPrice;
         }
 
-        public RailroadSpace(int _rentPrice, int _sellingPrice)
+        private bool mortgaged;
+        public override bool isMortgaged()
+        {
+            return mortgaged;
+        }
+
+        private Player owner;
+        public override Player ownedBy()
+        {
+            return owner;
+        }
+
+        public RailroadSpace(int _rentPrice, int _sellingPrice,
+            bool _mortgaged, Player _owner)
         {
             rentPrice = _rentPrice;
             sellingPrice = _sellingPrice;
@@ -114,7 +136,20 @@ namespace BusinessLayer
             return sellingPrice;
         }
 
-        public UtilitySpace(int _rentPrice, int _sellingPrice)
+        private bool mortgaged;
+        public override bool isMortgaged()
+        {
+            return mortgaged;
+        }
+
+        private Player owner;
+        public override Player ownedBy()
+        {
+            return owner;
+        }
+
+        public UtilitySpace(int _rentPrice, int _sellingPrice,
+            bool _mortgaged, Player _owner)
         {
             rentPrice = _rentPrice;
             sellingPrice = _sellingPrice;
